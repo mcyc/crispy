d3972b900ba3b8ed930650a5ca53749661b5dfa0
Mike Chen <mkid.chen@gmail.com>
2020-01-22 12:59:03 -0800

    created the setup.py file

---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="astro-crispy", # Replace with your own username
+    version="0.1.0",
+    author="Michael Chun-Yuan Chen",
+    author_email="mcychen@uvic.ca",
+    description="Computational Ridge Identification with SCMS for Python",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/mcyc/crispy",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 2",
+        "License :: OSI Approved :: GNU General Public License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=2.7',
+)

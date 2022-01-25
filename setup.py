f0de67af1e9d03cda0c551f883fee5d0a3a13427
Mike Chen <mkid.chen@gmail.com>
2022-01-24 20:15:42 -0500

    updated the version name in setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="astro-crispy", # Replace with your own username
-    version="0.2.0",
+    version="1.0.0",
     author="Michael Chun-Yuan Chen",
     author_email="chen.m@queensu.ca",
     description="Computational Ridge Identification with SCMS for Python",

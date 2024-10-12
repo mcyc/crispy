24e5052d74bc03de4d7bd2489aa2da1e73094bba
Mike Chen <mcyc@users.noreply.github.com>
2024-10-11 23:23:53 -0400

    Added requirements to the setup file

--- setup.py
+++ setup.py
@@ -1,22 +1,30 @@
-import setuptools
+from setuptools import setup, find_packages
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
-setuptools.setup(
+setup(
     name="astro-crispy", # Replace with your own username
-    version="1.2.0",
+    version="1.3.0",
     author="Michael Chun-Yuan Chen",
     author_email="chen.m@queensu.ca",
     description="Computational Ridge Identification with SCMS for Python",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/mcyc/crispy",
-    packages=setuptools.find_packages(),
+    packages=find_packages(),
+    install_requires=[
+        'numpy',
+        'scipy',
+        'astropy',
+        'matplotlib',
+        'scikit-learn',
+        'scikit-image',
+    ],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: GNU Lesser General Public License v3 (LGPLv3)",
         "Operating System :: OS Independent",
     ],
-    python_requires='>=3.3',
+    python_requires='>=3.8',
 )

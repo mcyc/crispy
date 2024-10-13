4a008f272c3af032fe53956148f925dc73694f67
Mike Chen <mcyc@users.noreply.github.com>
2024-10-12 20:57:40 -0400

    First working version of non-vectorized shift_particle() with numba

--- crispy/scms_numba.py
+++ crispy/scms_numba.py
@@ -1,5 +1,7 @@
 import numpy as np
+import numba
 
+@numba.njit(parallel=False, fastmath=True)
 def shift_particle(Gj, X, D, h, d, weights, n, H, Hinv):
     # shift individual walkers using SCMS
     c = np.zeros(weights.shape, dtype=weights.dtype) #(n,)
@@ -8,7 +10,8 @@ def shift_particle(Gj, X, D, h, d, weights, n, H, Hinv):
 
     for i, Xi in enumerate(X):  # Xi has shape (D,)
         # Evaluate the Gaussian value of Xi at Gj
-        c_i = np.exp(gaussian(Xi, mean=Gj, covariance=h ** 2))  # c is a scalar
+        #c_i = np.exp(gaussian(Xi, mean=Gj, covariance=h ** 2))  # c is a scalar
+        c_i = evaluate_gaussian(Xi, mean=Gj, covariance=h**2)
 
         # Now weight the probability of Xi by the image
         c[i] = c_i* weights[i]  # c is a scalar
@@ -19,6 +22,7 @@ def shift_particle(Gj, X, D, h, d, weights, n, H, Hinv):
         hh[i] = c[i]*np.dot(u_i, np.transpose(u_i)) - Hinv #(D,D)
 
     pj = np.mean(c)  # pj is a scalar
+    pj = np.float32(pj)
     g = -1 * np.sum(cu, axis=0) / n  # g has shape (D, 1)
 
     # Compute the Hessian matrix
@@ -34,11 +38,11 @@ def shift_particle(Gj, X, D, h, d, weights, n, H, Hinv):
     EigVal, EigVec = np.linalg.eigh(Sigmainv)  # EigVal has shape (D,), EigVec has shape (D, D)
 
     # Get the eigenvectors with the largest eigenvalues down to D-d
-    V = EigVec[:, d:D]  # V has shape (D, D-d)
+    V = np.ascontiguousarray(EigVec[:, d:D])  # V has shape (D, D-d) .astype(np.dtype('float32'), copy=False)
 
     # Compute V * V^T
     VT = np.transpose(V)
-    VVT = np.dot(V, np.transpose(V))  # VVT has shape (D, D)
+    VVT = np.dot(V, VT)  # VVT has shape (D, D)
 
     # Update Gj based on the projection
     Gj = np.dot(VVT, shift0 - Gj) + Gj  # Gj has shape (D,)
@@ -47,9 +51,9 @@ def shift_particle(Gj, X, D, h, d, weights, n, H, Hinv):
     tmp = np.dot(VT, g)  # tmp has shape (D-d,)
     errorj = np.sqrt(np.sum(tmp ** 2) / np.sum(g ** 2))  # errorj is a scalar
 
-    return Gj, errorj, pj, g, shift0
+    return Gj, errorj
 
-
+@numba.njit(parallel=False, fastmath=True)
 def gaussian(X, mean, covariance):
     """
     Compute N(X; mean, covariance) for a given point X in D-dimensional space
@@ -71,3 +75,24 @@ def gaussian(X, mean, covariance):
     deviations = X - mean
     inverses = 1 / covariance
     return -0.5 * (constant + log_determinants + np.sum(deviations * inverses * deviations))
+
+@numba.njit(parallel=False, fastmath=True)
+def evaluate_gaussian(X, mean, covariance):
+    """
+    Evaluate a D-dimensional Gaussian function at a given position.
+
+    Parameters:
+    X (numpy.ndarray): Position at which to evaluate the Gaussian, shape (D, 1).
+    mean (numpy.ndarray): Mean of the Gaussian, shape (D, 1).
+    covariance (float): Scalar covariance value (isotropic covariance, `covariance`).
+
+    Returns:
+    float: Value of the Gaussian function at position X.
+    """
+    D = mean.shape[0]
+    diff = X - mean
+    inv_covariance = 1 / covariance  # Since covariance is scalar, the inverse covariance matrix is just 1/covariance
+    normalization_factor = 1 / ((2 * np.pi * covariance) ** (D / 2))
+    exponent = -0.5 * np.dot(diff.T, diff) * inv_covariance
+    value = normalization_factor * np.exp(exponent)
+    return value.item()  # Return as a scalar
